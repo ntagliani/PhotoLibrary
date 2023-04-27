f2d93f71a410818b72bb338d927919fe98d581f0
Nicolas Tagliani <nicolas.tagliani@gmail.com>
2023-04-27 19:48:01 +0200

    Initial commit

---
+++ PhotoLibrary/DB/DeviceEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace PhotoLibrary.DB
+{
+    public class DeviceEntry
+    {
+        static DeviceEntry Create(String path)
+        {
+            var info = new FileInfo(path);
+            if (!info.Exists)
+            {
+                throw new FileNotFoundException(Resources.Messages_EN.FileNotFound, path);
+            }
+
+            var ret = new DeviceEntry();
+            ret.FileSize = (UInt64)info.Length;
+            ret.LastModifiedTime = info.LastWriteTimeUtc;
+            ret.Path = path;
+            return ret;
+        }
+
+        public String Path { get; private set; }
+        public DateTime LastModifiedTime { get; private set; }
+        public UInt64 FileSize { get; private set; }
+    }
+}
